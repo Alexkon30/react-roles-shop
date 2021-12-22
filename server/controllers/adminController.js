b19d244dcf98ef075144bb26ea02fd8b988c129c
Alexkon30 <cashish94@list.ru>
2021-12-23 01:19:01 +0300

    add ability to send email

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,55 +2,55 @@ import Order from '../models/OrderCard.js'
 import nodemailer from 'nodemailer'
 
 class adminController {
-
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            secure: false,
-            port: 587,
-            auth: {
-              user: "AlexKon243546@gmail.com",
-              pass: 'TestFront94.',
-            },
-          })
-    }
-
     async fetchAllOrders(_, res) {
         try {
             let orders = await Order.find()
-            res.send({success: true, orders})
+            res.send({ success: true, orders })
         } catch (e) {
-            res.send({success: false, message: 'Error with DB'})
+            res.send({ success: false, message: 'Error with DB' })
         }
     }
 
     async changeOrderStatus(req, res) {
         try {
-            let result  = await Order.findByIdAndUpdate(req.body.id, {isFinished: req.body.value}, {new: true})
-            // if (result.isFinished) {
-            //     this.transporter.sendMail({
-            //         from: 'AlexKon243546@gmail.com',
-            //         to: result.email,
-            //         subject: 'New order',
-            //         text: 'test',
-            //         html: `
-            //         <div>
-            //             <h3>New order</h3>
-            //             <ul>
-            //                 ${result.goods.map(item => `<li>${item}</li>`)}
-            //             </ul>
-            //             <p>Thanks for purchase!</p>
-            //         </div>
-            //         `
-            //     }, (err, res) => {
-            //         if (err) {
-            //             console.log(err)
-            //         }
-            //     })
-            // }
-            res.send({success: true, id: result.id})
+            let result = await Order.findByIdAndUpdate(req.body.id, { isFinished: req.body.value }, { new: true })
+
+            if (result.isFinished) {
+                let transporter = nodemailer.createTransport({
+                    host: "smtp.gmail.com",
+                    port: 587,
+                    secure: false,
+                    auth: {
+                        user: 'AlexKon243546@gmail.com',
+                        pass: 'TestFront94.',
+                    },
+                });
+
+                transporter.sendMail({
+                    from: 'AlexKon243546@gmail.com',
+                    to: result.email,
+                    subject: 'New order',
+                    text: '',
+                    html: `
+                    <div>
+                        <h3>New order</h3>
+                        <ul>
+                            ${result.goods.map(item => `<li>${item}</li>`)}
+                        </ul>
+                        <p>Thanks for purchase!</p>
+                    </div>
+                    `
+                }, (err) => {
+                    if (err) {
+                        return console.log(err)
+                    }
+                    res.send({ success: true, id: result.id })
+                })
+            } else {
+                res.send({ success: true, id: result.id })
+            }
         } catch (e) {
-            res.send({success: false, message: 'Error with DB'})
+            res.send({ success: false, message: 'Error with DB' })
         }
     }
 }
